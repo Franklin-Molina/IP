c1e837ccb99d1b4c42bd1909a3e6a901ec1a5cd6
cupongratis <lZzlxlzZl7@gmail.com>
2025-04-10 00:50:38 -0500

    authdelete

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy import create_engine
 from app.api.routes import router as url_router
-from app.api.auth import router as auth_router
 from app.core.database import Base, engine
 from app.core.middleware import RateLimitMiddleware
 
@@ -24,4 +23,4 @@ app.add_middleware(RateLimitMiddleware)
 
 # Routers
 app.include_router(url_router)
-app.include_router(auth_router)
+
